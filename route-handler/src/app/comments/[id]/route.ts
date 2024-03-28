cdb834f7c8ef6574b8e991136722f7b5b9cd53ce
ritikj27 <ritik.j@preciousinfosystem.com>
2024-03-28 16:49:34 +0530

    handle delete request

--- route-handler/src/app/comments/[id]/route.ts
+++ route-handler/src/app/comments/[id]/route.ts
@@ -16,3 +16,39 @@ export async function GET(
 
   return Response.json(comment);
 }
+
+export async function PATCH(
+  request: Request,
+  {
+    params,
+  }: {
+    params: {
+      id: string;
+    };
+  }
+) {
+  const body = await request.json();
+  const { text } = body;
+  const idx = comments.findIndex(
+    (comment) => comment.id === parseInt(params.id)
+  );
+  comments[idx].text = text;
+  return Response.json(comments[idx]);
+}
+
+export async function DELETE(
+  _request: Request,
+  {
+    params,
+  }: {
+    params: {
+      id: string;
+    };
+  }
+) {
+  const idx = comments.findIndex(
+    (comment) => comment.id === parseInt(params.id)
+  );
+  const deletedComment = comments.splice(idx, 1);
+  return Response.json(deletedComment);
+}
