83d3c7ef973d00ec877e1fd573835916093437c6
ritikj27 <ritik.j@preciousinfosystem.com>
2024-03-28 17:50:17 +0530

    headers route handler

--- route-handler/src/app/profile/api/route.ts
+++ route-handler/src/app/profile/api/route.ts
@@ -1,5 +1,17 @@
-export async function GET() {
-  return new Response("Profile data");
+import { headers } from "next/headers";
+import { NextRequest } from "next/server";
+
+export async function GET(request:NextRequest) {
+  const headersg = new Headers(request.headers);
+  const headersList = headers()
+
+  console.log(headersg.get("Authorization"));
+  console.log(headersList.get("Authorization"))
+  return new Response("<h1>hello world from profile</h1>",{
+    headers:{
+      "content-Type":"text/html"
+    }
+  });
 }
 
 
