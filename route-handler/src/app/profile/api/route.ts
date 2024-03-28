3f2ff99ff2887c0ab1bb667c165c771a811d09ab
ritikj27 <ritik.j@preciousinfosystem.com>
2024-03-28 18:20:19 +0530

    cookies in route handler

--- route-handler/src/app/profile/api/route.ts
+++ route-handler/src/app/profile/api/route.ts
@@ -1,15 +1,20 @@
-import { headers } from "next/headers";
+import { headers ,cookies} from "next/headers";
 import { NextRequest } from "next/server";
 
 export async function GET(request:NextRequest) {
   const headersg = new Headers(request.headers);
   const headersList = headers()
 
+  const theme = request.cookies.get("theme")
+  console.log(theme)
+  cookies().set("ok","201")
+
   console.log(headersg.get("Authorization"));
   console.log(headersList.get("Authorization"))
   return new Response("<h1>hello world from profile</h1>",{
     headers:{
-      "content-Type":"text/html"
+      "content-Type":"text/html",
+      "set-cookie":"theme=dark"
     }
   });
 }
