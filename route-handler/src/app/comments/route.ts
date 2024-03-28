355acb8b95d534fcff88f4d7aedde5c5129e3c76
ritikj27 <ritik.j@preciousinfosystem.com>
2024-03-28 17:23:26 +0530

    redirect in route handler

--- route-handler/src/app/comments/route.ts
+++ route-handler/src/app/comments/route.ts
@@ -1,9 +1,6 @@
 import { type NextRequest } from "next/server";
 import { comments } from "./data";
 
-
-
-
 export async function GET(request:NextRequest) {
   const searchParams = request.nextUrl.searchParams;
   const query = searchParams.get("query")
