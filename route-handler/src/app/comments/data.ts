828734534aa77e43da32f2a080618bd6b0bdc3f1
ritikj27 <ritik.j@preciousinfosystem.com>
2024-03-28 15:13:01 +0530

    get request handler

---
+++ route-handler/src/app/comments/data.ts
@@ -0,0 +1,14 @@
+export const comments= [
+    {
+        id:1,
+        text:"hello world",
+    },
+    {
+        id:2,
+        text:"nothing"
+    },
+    {
+        id:3,
+        text:"pro coder",
+    }
+]
